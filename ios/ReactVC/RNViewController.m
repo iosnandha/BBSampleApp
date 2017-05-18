780f479bd7a7c6a2ae7520fff4bf8cba45c1eb4f
Nandagopalan - BG Mobile App Transformation <snanthagopal@gmail.com>
2017-05-18 16:25:35 +0100

    Xcode PBX Changes

--- ios/ReactVC/RNViewController.m
+++ ios/ReactVC/RNViewController.m
@@ -33,15 +33,13 @@
                                    moduleProvider:nil
                                     launchOptions:nil];
     
-    
     RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"BBSampleApp" initialProperties:props];
-    
     
     rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
     
     [self.view addSubview:rootView];
     rootView.frame = self.view.bounds;
-    
+    //Testing
 }
 
 - (void)didReceiveMemoryWarning {
